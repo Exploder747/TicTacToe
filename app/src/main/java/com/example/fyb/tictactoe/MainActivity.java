c39182f2147999851d7269246d18f26e5b5ce469
Exploder <yaddayadda@somewhat.com>
2017-05-11 21:33:56 +0300

    main project

---
+++ app/src/main/java/com/example/fyb/tictactoe/MainActivity.java
@@ -0,0 +1,213 @@
+package com.example.fyb.tictactoe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int c[][];
+    int i, j, k = 0;
+    Button b[][];
+    TextView textView;
+    Enemy enemy;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        crBoard();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuItem item = menu.add("New Game");
+        return true;
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        crBoard();
+        return true;
+    }
+
+    private void crBoard() {
+        enemy = new Enemy();
+        b = new Button[4][4];
+        c = new int[4][4];
+
+        textView = (TextView) findViewById(R.id.textView);
+
+        b[1][3] = (Button) findViewById(R.id.f1);
+        b[1][2] = (Button) findViewById(R.id.f2);
+        b[1][1] = (Button) findViewById(R.id.f3);
+
+        b[2][3] = (Button) findViewById(R.id.f4);
+        b[2][2] = (Button) findViewById(R.id.f5);
+        b[2][1] = (Button) findViewById(R.id.f6);
+
+        b[3][3] = (Button) findViewById(R.id.f7);
+        b[3][2] = (Button) findViewById(R.id.f8);
+        b[3][1] = (Button) findViewById(R.id.f9);
+
+        for (i = 1; i <= 3; i++) {
+            for (j = 1; j <= 3; j++)
+                c[i][j] = 2;
+        }
+
+        textView.setText("Click a button to start");
+
+        for (i = 1; i <= 3; i++) {
+            for (j = 1; j <= 3; j++) {
+                b[i][j].setOnClickListener(new CustomClickListener(i, j));
+                if (!b[i][j].isEnabled()) {
+                    b[i][j].setText(" ");
+                    b[i][j].setEnabled(true);
+                }
+            }
+        }
+    }
+
+
+    class CustomClickListener implements View.OnClickListener {
+        int x;
+        int y;
+
+
+        public CustomClickListener(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public void onClick(View view) {
+            if (b[x][y].isEnabled()) {
+                b[x][y].setEnabled(false);
+                b[x][y].setText("O");
+                c[x][y] = 0;
+                textView.setText("");
+                if (!ckBoard()) {
+                    enemy.takeTurn();
+                }
+            }
+        }
+    }
+
+    private class Enemy {
+        public void takeTurn() {
+            if (c[1][1] == 2 &&
+                    ((c[1][2] == 0 && c[1][3] == 0) ||
+                            (c[2][2] == 0 && c[3][3] == 0) ||
+                            (c[2][1] == 0 && c[3][1] == 0))) {
+                markField(1, 1);
+            } else if (c[1][2] == 2 &&
+                    ((c[2][2] == 0 && c[3][2] == 0) ||
+                            (c[1][1] == 0 && c[1][3] == 0))) {
+                markField(1, 2);
+            } else if (c[1][3] == 2 &&
+                    ((c[1][1] == 0 && c[1][2] == 0) ||
+                            (c[3][1] == 0 && c[2][2] == 0) ||
+                            (c[2][3] == 0 && c[3][3] == 0))) {
+                markField(1, 3);
+            } else if (c[2][1] == 2 &&
+                    ((c[2][2] == 0 && c[2][3] == 0) ||
+                            (c[1][1] == 0 && c[3][1] == 0))) {
+                markField(2, 1);
+            } else if (c[2][2] == 2 &&
+                    ((c[1][1] == 0 && c[3][3] == 0) ||
+                            (c[1][2] == 0 && c[3][2] == 0) ||
+                            (c[3][1] == 0 && c[1][3] == 0) ||
+                            (c[2][1] == 0 && c[2][3] == 0))) {
+                markField(2, 2);
+            } else if (c[2][3] == 2 &&
+                    ((c[2][1] == 0 && c[2][2] == 0) ||
+                            (c[1][3] == 0 && c[3][3] == 0))) {
+                markField(2, 3);
+            } else if (c[3][1] == 2 &&
+                    ((c[1][1] == 0 && c[2][1] == 0) ||
+                            (c[3][2] == 0 && c[3][3] == 0) ||
+                            (c[2][2] == 0 && c[1][3] == 0))) {
+                markField(3, 1);
+            } else if (c[3][2] == 2 &&
+                    ((c[1][2] == 0 && c[2][2] == 0) ||
+                            (c[3][1] == 0 && c[3][3] == 0))) {
+                markField(3, 2);
+            } else if (c[3][3] == 2 &&
+                    ((c[1][1] == 0 && c[2][2] == 0) ||
+                            (c[1][3] == 0 && c[2][3] == 0) ||
+                            (c[3][1] == 0 && c[3][2] == 0))) {
+                markField(3, 3);
+            } else {
+                Random rand = new Random();
+
+                int a = rand.nextInt(4);
+                int b = rand.nextInt(4);
+                while (a == 0 || b == 0 || c[a][b] != 2) {
+                    a = rand.nextInt(4);
+                    b = rand.nextInt(4);
+                }
+                markField(a, b);
+            }
+        }
+
+
+    }
+
+    private void markField(int x, int y) {
+        b[x][y].setEnabled(false);
+        b[x][y].setText("X");
+        c[x][y] = 1;
+        ckBoard();
+    }
+
+
+    private boolean ckBoard() {
+        boolean gameOver = false;
+        if ((c[1][1] == 0 && c[2][2] == 0 && c[3][3] == 0)
+                || (c[1][3] == 0 && c[2][2] == 0 && c[3][1] == 0)
+                || (c[1][2] == 0 && c[2][2] == 0 && c[3][2] == 0)
+                || (c[1][3] == 0 && c[2][3] == 0 && c[3][3] == 0)
+                || (c[1][1] == 0 && c[1][2] == 0 && c[1][3] == 0)
+                || (c[2][1] == 0 && c[2][2] == 0 && c[2][3] == 0)
+                || (c[3][1] == 0 && c[3][2] == 0 && c[3][3] == 0)
+                || (c[1][1] == 0 && c[2][1] == 0 && c[3][1] == 0)) {
+            textView.setText("Game over. You win!");
+            gameOver = true;
+        } else if ((c[1][1] == 1 && c[2][2] == 1 && c[3][3] == 1)
+                || (c[1][3] == 1 && c[2][2] == 1 && c[3][1] == 1)
+                || (c[1][2] == 1 && c[2][2] == 1 && c[3][2] == 1)
+                || (c[1][3] == 1 && c[2][3] == 1 && c[3][3] == 1)
+                || (c[1][1] == 1 && c[1][2] == 1 && c[1][3] == 1)
+                || (c[2][1] == 1 && c[2][2] == 1 && c[2][3] == 1)
+                || (c[3][1] == 1 && c[3][2] == 1 && c[3][3] == 1)
+                || (c[1][1] == 1 && c[2][1] == 1 && c[3][1] == 1)) {
+            textView.setText("Game over. You lost!");
+            gameOver = true;
+        } else {
+            boolean empty = false;
+            for(i=1; i<=3; i++) {
+                for(j=1; j<=3; j++) {
+                    if(c[i][j]==2) {
+                        empty = true;
+                        break;
+                    }
+                }
+            }
+            if(!empty) {
+                gameOver = true;
+                textView.setText("Game over. It's a draw!");
+            }
+        }
+        return gameOver;
+    }
+}
+
